bff96e93d14c7c577f8d26461437b7f8abac3aee
tigawanna <denniskinuthiawaweru@gmail.com>
2024-08-08 19:47:33 +0300

    romove chat route

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-```
-deno task start
-```
+
--- routes/candidate/index.ts
+++ routes/candidate/index.ts
@@ -15,72 +15,6 @@ app.get("/", (c) => {
   return c.json({ message: "candidate route" });
 });
 
-// app.post("/summarize", async (c) => {
-//   try {
-//     const authHeader = c.req.header("Authorization");
-//     if (!authHeader || authHeader.length < 10) {
-//       return c.json({ message: "Unauthorized" }, 401);
-//     }
-//     const supabaseClient = createClient<Database>(Deno.env.get("SUPABASE_URL") ?? "", authHeader, {
-//       global: { headers: { Authorization: authHeader } },
-//     });
-//     const { record } = await c.req.json<SummarizeRequestBody>();
-//     if (!record) {
-//       return c.json({ message: "payload should be provided" }, 400);
-//     }
-//     if (!record?.id) {
-//       return c.json({ message: "payload record id should be provided" }, 400);
-//     }
-//     const raw_string_input = validateCandidate(record);
-//     if (raw_string_input instanceof Response) {
-//       return raw_string_input;
-//     }
-
-//     //  get candidate's most recent spirations
-//     const candidateContext = await getCandidateContextFromRecord({ sb: supabaseClient, record });
-//     const summary_response = await generateCandidateVibeSummary({
-//       inputText: candidateContext,
-//     });
-//     const summary_text = summary_response.response.text();
-//     // early return if summary generation failed
-//     if (!summary_text) {
-//       return c.json({ message: "candidate summary generation failed" }, 400);
-//     }
-//     const embeddingResult = await geminiEmbedding({
-//       inputText: candidateContext,
-//     });
-//     // update candidate summary
-//     const { error: updateCandidateSummaryError } = await updateCandidate({
-//       sb: supabaseClient,
-//       record: {
-//         id: record.id,
-//         candidate_summary: summary_text,
-//       },
-//     });
-
-//     if (updateCandidateSummaryError) {
-//       return c.json(
-//         { message: "candidate summary update failed" + updateCandidateSummaryError.message },
-//         400
-//       );
-//     }
-//     // update candidate embedding
-//     const { error: updateCandidateEmbeddingError } = await updateCandidate({
-//       sb: supabaseClient,
-//       //@ts-expect-error: this might be wrong
-//       record: { id: record.id, embedding: embeddingResult.embedding.values },
-//     });
-//     if (updateCandidateEmbeddingError) {
-//       return c.json(
-//         { message: "candidate embedding update failed" + updateCandidateEmbeddingError.message },
-//         400
-//       );
-//     }
-//     return c.json({ message: "candidate  summary + embedding updated successfully" }, 200);
-//   } catch (error) {
-//     return c.json({ message: "Something went wrong: " + error.message }, 500);
-//   }
-// });
 app.post("/embed", async (c) => {
   try {
     const authHeader = c.req.header("Authorization");
@@ -129,9 +63,7 @@ interface ChatRequestBody {
   prompt: string;
   context_text: string; // this should be info about the candidate's most recent spirations together with any chat history if vailabe
 }
-app.get("/chat", (c) => {
-  return c.json({ message: "chat route" });
-});
+
 app.post("/chat", async (c) => {
   const kv = await Deno.openKv();
   await ipRateLimit({ c, kv });
